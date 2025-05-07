360f2c2f91e0e935a1c5c50556bcd24c70d574ac
Faish2011 <faishalabdurrahman2024@gmail.com>
2025-05-07 13:57:41 +0700

    all

---
+++ string-color-python.py
@@ -0,0 +1,52 @@
+from stringcolor import*
+print (cs("here we go", "orchid"))
+print (cs("away to space!", "DeepPink3"))
+print (cs("final fantasy", "#ffff87"))
+
+print (cs("purple number 4 bold", "purple4").bold())
+print (cs("blue, underlined", "blue").underline())
+print (bold("bold and underlined!").underline().cs ("red", "gold"))
+print (underline ("the bottom line."))
+print ()
+
+
+print (cs("warning!", "yellow", "#ff0000"))
+print ()
+
+print (cs("wild", "pink") +" stuff")
+print ("nothing "+cs("something", "DarkViolet2", "lightgrey6"))
+print ()
+
+print (cs("this will show up red", "#ff0009"))
+print (cs("so will this", "rgb(254, 0, 1)"))
+print ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
